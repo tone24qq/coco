9235c94941b6e16ffdec4e19a609c6880360da53
tone24qq <tone24qq@gmail.com>
2025-06-01 18:50:36 +0800

    更新 analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -7,7 +7,9 @@ import logging
 from pydantic import BaseModel, Field, model_validator # model_validator 新增
 
 # 來源：brain.py (本项目)
-import brain
+import brain1
+import brain2
+import brain3
 
 # 來源：main.py (用户需求 Point 4.c)
 logger = logging.getLogger(__name__) # 建議使用 __name__ 以便日誌追蹤來源模組
