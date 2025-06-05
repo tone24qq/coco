1cf9ec9883487f543e99fbbabc1c4df83247bea3
tone24qq <tone24qq@gmail.com>
2025-06-06 03:25:49 +0800

    Update main14_optimized.py

--- main14_optimized.py
+++ main14_optimized.py
@@ -1,5 +1,4 @@
 # main14_optimized.py
-
 from vectorized_brain_modules import VectorizedBrainModules
 import os
 import logging
@@ -16,7 +15,7 @@ from pydantic import BaseModel, Field
 from typing import List, Tuple, Any
 
 from analyzer11_optimized import analyze_with_prior
-+from vectorized_brain_modules import VectorizedBrainModules
+from vectorized_brain_modules import VectorizedBrainModules
 
 # Environment constants
 # TODO: 若要修改值，請透過環境變數設定
